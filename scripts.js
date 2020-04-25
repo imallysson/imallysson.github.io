084ce78e3107aeb658537967ff2ef16b784dd86c
Allysson Matheus dos Santos <allysson.lp@gmail.com>
2020-04-25 12:37:19 +0200

    Added meta tags and a vanilla dark/light theme.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
-//no scripts this time :)
+const btnChangeTheme = document.getElementById("change-theme");
+let isLight = getThemeFromStorage();
+
+if (isLight) {
+  toggleTheme();
+}
+
+function getThemeFromStorage() {
+  const isLightTheme = parseInt(localStorage.getItem('isLightTheme'));
+  
+  return isLightTheme === 1 ? true : false;
+}
+
+
+function changeTheme() {
+  isLight = !isLight;
+  localStorage.setItem('isLightTheme', String(isLight ? 1 : 0));
+  
+  toggleTheme();
+}
+
+function toggleTheme() {
+  btnChangeTheme.innerHTML = isLight ? '🌑' : '☀️';
+
+  document.body.classList.toggle('light');
+  document.getElementById("name").classList.toggle('light');
+  document.getElementById('title').classList.toggle('light');
+  const links = document.getElementsByClassName('link');
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.toggle('light');  
+  }
+}
+
+btnChangeTheme.addEventListener('click', changeTheme);
